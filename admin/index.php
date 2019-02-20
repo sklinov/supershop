143c5979a21eac74caa6171bfa5b547d4d92cf6d
sklinov <sergeyklinov@gmail.com>
2019-02-20 15:59:25 +0500

    1

--- admin/index.php
+++ admin/index.php
@@ -1,9 +1,30 @@
 <!DOCTYPE html>
-<html>
+<html lang="ru">
 <head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="styles/admin.css">
+	<script src="scripts/jquery-3.3.1.min.js"></script>
+	<script src="scripts/jquery.maskedinput.js"></script>
+	<script src="scripts/script.js"></script>
 	<title>Super Shop</title>
 </head>
-<body>
+<body class="page">
+	<div class="container">
+		<div class="menu">
+			<div class="menu__logo">Super SHOP</div>
+			<div class="menu__item">Заказы</div>
+			<div class="menu__item">Покупатели</div>
+			<div class="menu__item">Товары</div>
+			<div class="menu__item">Категории</div>
+			<div class="menu__footer">admin@mail.ru</div>
+			<div class="menu__footer">выйти</div>
+		</div>
+		<div class="content">
+			<h1>Заголовок</h1>
+			
+		</div>
+	</div>
 
 </body>
 </html>
